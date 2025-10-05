57ae1d1582596e12cb96a6d6221d849aa6d802e0
lauraleao01 <lauraleao2001@gmail.com>
2025-10-05 17:45:48 +0000

    incremento e decremento

---
+++ Testes/Incremento.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main (){
+    /*
+    Incremento (++)
+    Pré-Incremento ++a
+    Pós-Incremento a++
+    Decremento (--)
+    Pré-Decremento --a
+    Pós-Decremento a--
+    */
+   
+    int numero1 = 1, resultado;
+
+    printf("Antes incremento: %d\n", numero1);
+    //numero1 = numero1 + 1;
+    //numero1 += 1;
+    //Pos incremento:
+    //resultado =  numero1
+    //numero++
+    resultado = numero1++;
+    //printf("Apos incremento: %d\n", numero1);
+    printf("Apos Pós-incremento - Numero 1; %d - Resultado: %d\n", numero1, resultado);
+
+    resultado = ++numero1;
+    printf("Apos Prè-incremento - Numero 1; %d - Resultado: %d\n", numero1, resultado);
+    
+    //numero1 = numero1 - 1;
+    //numero1 -= 1;
+    //numero1--;
+    //printf("Apos decremento: %d\n", numero1);
+    resultado = numero1--;
+    printf("Apos Pos- decremento - Numero 1; %d - Resultado: %d\n", numero1, resultado);
+
+    resultado = --numero1;
+    printf("Apos Pré- decremento - Numero 1; %d - Resultado: %d\n", numero1, resultado);
+
+}
